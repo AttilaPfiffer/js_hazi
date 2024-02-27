4635b944ed985c2b26f252c738c4df86accabf29
AttilaPfiffer <pifuati@gmail.com>
2024-02-27 15:50:45 +0100

    hazi

---
+++ main.js
@@ -0,0 +1,58 @@
+const jegyek = [   
+    {
+        banda: "Avenged Sevenfold",
+        ar: 18000,
+        datum: 2024,
+        helyszin: "Wien",
+        vip: "VIP: Igen",
+    },
+
+    {
+        banda: "3 Doors Down",
+        ar: 9000,
+        datum: 2025,
+        helyszin: "Houston, Texas",
+        vip: "VIP: Nem",
+    },
+
+    {
+        banda: "Metallica",
+        ar: 20000,
+        datum: 2024,
+        helyszin: "Paris",
+        vip: "VIP: Igen",
+    },
+
+    {
+        banda: "Slipknot",
+        ar: 25000,
+        datum: 2025,
+        helyszin: "Berlin",
+        vip: "VIP: Nem",
+    }
+]
+
+
+console.log(jegyek[0].banda)
+console.log(jegyek[0].ar)
+console.log(jegyek[0].datum)
+console.log(jegyek[0].helyszin)
+console.log(jegyek[0].vip)
+console.log(" ")
+console.log(jegyek[1].banda)
+console.log(jegyek[1].ar)
+console.log(jegyek[1].datum)
+console.log(jegyek[1].helyszin)
+console.log(jegyek[1].vip)
+console.log(" ")
+console.log(jegyek[2].banda)
+console.log(jegyek[2].ar)
+console.log(jegyek[2].datum)
+console.log(jegyek[2].helyszin)
+console.log(jegyek[2].vip)
+console.log(" ")
+console.log(jegyek[3].banda)
+console.log(jegyek[3].ar)
+console.log(jegyek[3].datum)
+console.log(jegyek[3].helyszin)
+console.log(jegyek[3].vip)
